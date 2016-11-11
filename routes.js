9c53d74a461d0e0ce36d830cc3aeeed04d4b7b3b
jessicamann <jmann1163@gmail.com>
2016-11-11 15:48:15 -0600

    add route for default to return 200

--- routes.js
+++ routes.js
@@ -4,6 +4,9 @@ const controller = require('./controller');
 module.exports = function(app) {
 	const routes = express.Router();
 
+	routes.get('/', (request, response) => {
+		response.status(200);
+	});
 	routes.get('/api', (request, response) => {
 		response.status(200).json({message: 'Connected!'});
 	});
