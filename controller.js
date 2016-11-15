d3008c5fd4b51c3a5edab389e596a88e155d3d58
jessicamann <jmann1163@gmail.com>
2016-11-15 10:30:30 -0600

    add database connection

---
+++ controller.js
@@ -0,0 +1,25 @@
+var promise = require('bluebird');
+
+var options = {
+  // Initialization Options
+  promiseLib: promise
+};
+
+var pgp = require('pg-promise')(options);
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/data';
+var db = pgp(connectionString);
+
+exports.getAllPlaces = function(req, res, next) {
+  db.any('select * from places')
+      .then(function (data) {
+        res.status(200)
+          .json({
+            status: 'success',
+            data: data,
+            message: 'Retrieved ALL places'
+          });
+      })
+      .catch(function (err) {
+        return next(err);
+      });
+}
--- routes.js
+++ routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const controller = require('./controller');
 
 module.exports = function(app) {
 	const routes = express.Router();
@@ -6,6 +7,8 @@ module.exports = function(app) {
 	routes.get('/api', (request, response) => {
 		response.status(200).json({message: 'Connected!'});
 	});
+
+	routes.get('/places', controller.getAllPlaces);
 
 	app.use('/', routes);
 }
